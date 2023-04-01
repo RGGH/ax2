9ae8f010d8b0543a9e9a6a259735e01b6b792168
RGGH <pisscompany@gmail.com>
2023-04-01 12:27:26 +0100

    Added Query

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,12 @@ async fn second_handler() -> Json<Message> {
     })
 }
 
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct QueryParams {
-    message : String,
-    id : i32,
+    message: String,
+    id: i32,
 }
 
-pub async fn query_params(Query(query): Query<QueryParams>)->Json<QueryParams>{
+pub async fn query_params(Query(query): Query<QueryParams>) -> Json<QueryParams> {
     Json(query)
 }
