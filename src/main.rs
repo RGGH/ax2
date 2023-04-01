cf098e0e08fac6895a876e1666788e1722dd6030
RGGH <pisscompany@gmail.com>
2023-04-01 10:35:42 +0100

    params work

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use axum::{routing::get, routing::post, Json, Router,response::Html,extract::Query};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().
+    route("/", get(handler))
+    .route("/foo", get(second_handler))
+    .route("/bar", post(get_param_with_multi_values));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+
+    println!("Server started, listening on {addr}");
+    
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+#[derive(serde::Serialize)]
+struct Message {
+    message: String,
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn second_handler() -> Json<Message> {
+    Json(Message {
+        message: String::from("Hello, World2!"),
+    })
+}
+
+async fn get_param_with_multi_values(
+    Query(params): Query<Vec<(String, String)>>
+) -> String {
+    format!("params: {:?}", params)
+}
